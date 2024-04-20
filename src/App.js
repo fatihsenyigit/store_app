b09e5080f6ea44b51fd5532fc19656cc42de1dc2
fatih <fsenyigit8@gmail.com>
2024-04-20 12:43:02 +0300

    login page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 
+import { AppRouter } from './router/AppRouter';
 
 function App() {
   return (
-    <div className="min-h-screen bg-slate-700 text-white text-4xl flex justify-center gap-2 items-center">
-      <span className="animate-bounce">🏀</span>
-      <h1 className="animate-pulse">
-        Welcome to Session 1 of the Clarus Store Project FS16!
-      </h1>
-      <span className="animate-bounce">🏀</span>
+    <div>
+      
+   <AppRouter></AppRouter>
     </div>
   );
 }
