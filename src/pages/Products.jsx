145a47f4aa08b4be79dd4838907a353a1dbd4c7c
fatih <fsenyigit8@gmail.com>
2024-05-14 09:18:30 +0300

    product card

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import axios from "axios"
+import { useEffect, useState } from "react"
+import ProductCard from "../components/ProductCard"
+
 
 const Products = () => {
+  const [products, setProducts] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [search, setSearch] = useState("");
+
+  const getData = async() => {
+    try {
+      const {data}= await axios.get(
+        `https://dummyjson.com/products/search?q=${search}`
+      );
+      setProducts(data.products)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+  getData();
+  }, []);
+  
   return (
-    <div>Products</div>
+    <div>
+      <ProductCard/>
+    </div>
   )
 }
 
