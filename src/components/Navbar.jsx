33481c84cb79283fd5c0aef3744922976ee20f3e
fatih <fsenyigit8@gmail.com>
2024-05-14 08:13:55 +0300

    hamburger button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,8 @@
 import { NavLink } from "react-router-dom";
 import logo from "../assets/logo.png";
 import { closeNavbar, logoutIcon, openNavbar } from "../helpers/icons";
+import { useState } from "react";
+
 
 const navigation = [
   {
@@ -19,6 +21,7 @@ const navigation = [
 ];
 
 const Navbar = () => {
+    const [show, setShow] = useState(false);
   return (
     <nav className="bg-navbarColor md:text-sm">
       <div className="gap-x-14 items-center max-w-screen-xl mx-auto px-4 md:flex md:px-8">
@@ -33,9 +36,21 @@ const Navbar = () => {
               Store App
             </span>
           </a>
+          <div className="md:hidden">
+            <button
+              className="menu-btn text-gray-500 hover:text-gray-800"
+              onClick={() => setShow(!show)}
+            >
+              {show ? closeNavbar : openNavbar}
+            </button>
+          </div>
         </div>
-        <div className="flex flex-1 items-center">
-          <ul className="space-y-6 flex md:space-x-6 md:space-y-6">
+        <div
+          className={`${
+            show ? "flex flex-col pb-2" : "hidden"
+          } md:flex md:flex-row flex-1 items-center `}
+        >
+          <ul className="md:flex md:space-x-6">
             {navigation.map((item) => (
               <li
                 key={item.title}
@@ -50,7 +65,7 @@ const Navbar = () => {
           <div className="flex-1 gap-x-6 items-center justify-end mt-6 space-y-6 md:flex md:space-y-0 md:mt-0">
             <NavLink
               // to="/"
-              onClick={() => logout()}
+              //   onClick={() => logout()}
               className="flex items-center justify-center gap-x-1 py-2 px-4 font-medium text-gray-700 hover:bg-main hover:text-white active:bg-gray-900 rounded-full md:inline-flex"
             >
               Logout {logoutIcon}
