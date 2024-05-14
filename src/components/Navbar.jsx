f3902085a9a6c833e402c6adf31abfb57eb2972d
fatih <fsenyigit8@gmail.com>
2024-05-14 09:05:56 +0300

    session storage for refresh page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,8 @@
 import { NavLink } from "react-router-dom";
 import logo from "../assets/logo.png";
 import { closeNavbar, logoutIcon, openNavbar } from "../helpers/icons";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { AuthContext } from "../context/AuthProvider";
 
 
 const navigation = [
@@ -22,6 +23,7 @@ const navigation = [
 
 const Navbar = () => {
     const [show, setShow] = useState(false);
+    const { logout } = useContext(AuthContext);
   return (
     <nav className="bg-navbarColor md:text-sm">
       <div className="gap-x-14 items-center max-w-screen-xl mx-auto px-4 md:flex md:px-8">
@@ -64,8 +66,7 @@ const Navbar = () => {
           </ul>
           <div className="flex-1 gap-x-6 items-center justify-end mt-6 space-y-6 md:flex md:space-y-0 md:mt-0">
             <NavLink
-              // to="/"
-              //   onClick={() => logout()}
+                onClick={() => logout()}
               className="flex items-center justify-center gap-x-1 py-2 px-4 font-medium text-gray-700 hover:bg-main hover:text-white active:bg-gray-900 rounded-full md:inline-flex"
             >
               Logout {logoutIcon}
--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,19 +1,28 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 export const AuthContext = createContext()
 // const {Provider} = createContext()
 
 const AuthProvider = ({children}) => {
-    const [user, setUser] = useState()
+    const [user, setUser] = useState(
+      JSON.parse(sessionStorage.getItem("user")) || null,
+    );
     const navigate = useNavigate()
 
     const login = (info) => {
       setUser(info)
       navigate('/dashboard')
     }
+
+    const logout = () => setUser(null);
+
+      useEffect(() => {
+        sessionStorage.setItem("user", JSON.stringify(user));
+      }, [user]);
+
   return (
-    <AuthContext.Provider value={{user, login}}>
+    <AuthContext.Provider value={{user, login, logout}}>
         {children}
     </AuthContext.Provider>
 
