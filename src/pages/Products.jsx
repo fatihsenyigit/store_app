3c9464549f6021333d8e1920e2447ff78e7a931e
fatih <fsenyigit8@gmail.com>
2024-05-14 15:27:06 +0300

    loading

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,33 +1,46 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
-import ProductCard from "../components/ProductCard"
-
+import axios from "axios";
+import { useEffect, useState } from "react";
+import ProductCard from "../components/ProductCard";
+import Loading from "../components/Loading";
 
 const Products = () => {
-  const [products, setProducts] = useState([])
-  const [loading, setLoading] = useState(false)
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
   const [search, setSearch] = useState("");
 
-  const getData = async() => {
+  const getData = async () => {
     try {
-      const {data}= await axios.get(
-        `https://dummyjson.com/products/search?q=${search}`
+      const { data } = await axios.get(
+        `https://dummyjson.com/products/search?q=${search}`,
       );
-      setProducts(data.products)
+      setProducts(data.products);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   useEffect(() => {
-  getData();
+    getData();
   }, []);
-  
-  return (
-    <div>
-      <ProductCard/>
-    </div>
-  )
-}
 
-export default Products
+  return (
+    <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+      <h2 className="text-2xl font-bold mt-8 tracking-tighter text-gray-900">
+        All Products
+      </h2>
+      {loading ? (
+        <Loading />
+      ) : products.length ? (
+        <div className="mt-6 grid grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10 xl:gap-x-8">
+          {products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+      ) : (
+        <h2 className="text-center text-3xl text-red-600 mt-32">No Products</h2>
+      )}
+    </div>
+  );
+};
+
+export default Products;
