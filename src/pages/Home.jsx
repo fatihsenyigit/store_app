356b2a99c547346bb9e1a720c09ede5ba8b68bce
fatih <fsenyigit8@gmail.com>
2024-05-14 20:51:29 +0300

    stats and testimonial

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Testimonial from "../components/Testimonial";
+import Stats from "../components/Stats";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
+    <div>
+      <Stats />
+      <Testimonial />
+    </div>
+  );
 }
 
 export default Home
